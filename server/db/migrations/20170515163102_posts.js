3d33c43e7ee3ef058788bc1bddd46b2363fdefca
jeannebardin <jmbardin@gmail.com>
2022-07-11 17:17:57 +1200

    Initial commit

--- server/db/migrations/20170515163102_posts.js
+++ server/db/migrations/20170515163102_posts.js
@@ -11,3 +11,32 @@ exports.up = (knex) => {
 exports.down = (knex) => {
   return knex.schema.dropTable('Posts')
 }
+
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+
+//  exports.up = (knex) => {
+//   return knex.schema.createTable('Posts', (table) => {
+//     table.increments().primary()
+//     table.string('title')
+//     table.string('content')
+//     table.date('date_created')
+//     table.integer('vote_count').defaultsTo(0)
+//     table.string('paragraphs')
+//     // Paragraphs in output.json is "full_text"
+//     table.string('categories')
+//   })
+// }
+
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+
+// exports.down = (knex) => {
+//   return knex.schema.dropTable('Posts')
+// }
+
+
